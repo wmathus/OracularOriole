a76a92e4f5cbde0fccdbb7aaac29f0f0417c0ecc
Aidaparvaneh <a.parvaneh@se24.qmul.ac.uk>
2025-02-18 09:28:50 +0000

    Update and rename config.py to src/config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',  # Change this if you use a different MySQL username
-    'password': 'password',  # Replace with your actual MySQL password
-    'database': 'ff'}
---
+++ src/config.py
@@ -0,0 +1,5 @@
+DB_CONFIG = {
+    'host': 'sql8.freesqldatabase.com',
+    'user': 'sql8762368',  # Change this if you use a different MySQL username
+    'password': 'DB45Pl1pRv',  # Replace with your actual MySQL password
+    'database': 'sql8762368'}
